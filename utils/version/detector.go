d25c17f5fb1a0a4c7474555edc7522250ac2741f
Tiger Wang <tigerwang@outlook.com>
2022-08-10 03:23:54 +0000

    wip

--- utils/version/detector.go
+++ utils/version/detector.go
@@ -1,11 +1,23 @@
 package version
 
+// Get version of CasaOS by calling API.
+func GetVersionFromAPI() (string, error) {
+	// TODO
+	return "", nil
+}
+
+// Detect minor version of CasaOS. It returns 2 for "0.2.x" or 3 for "0.3.x"
+//
+// (This is often useful when failing to get version from API because CasaOS is not running.)
 func DetectMinorVersion() (int, error) {
-	// return 2 for 0.2.x or 3 for 0.3.x
+	// TODO
 	return 0, nil
 }
 
-func DBbasedUserData() (bool, error) {
-	// return true if user data is stored in database (0.3.3-0.3.5), false if config file (0.3.0-0.3.2)
+// Check if user data is stored in database (true) or in config file (false)
+//
+// (user data is stored in config file for 0.3.0-0.3.2)
+func IsUserDataInDatabase() (bool, error) {
+	// TODO
 	return false, nil
 }
