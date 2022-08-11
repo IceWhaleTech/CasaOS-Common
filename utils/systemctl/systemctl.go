9b3941b9154c99aabb8e0cba73a5a233bd8bb39c
Tiger Wang <tigerwang@outlook.com>
2022-08-11 02:26:24 +0000

    wip

--- utils/systemctl/systemctl.go
+++ utils/systemctl/systemctl.go
@@ -7,6 +7,30 @@ import (
 	"github.com/coreos/go-systemd/v22/dbus"
 )
 
+func EnableService(name string) error {
+	// connect to systemd
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	conn, err := dbus.NewSystemdConnectionContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	_, fileChanges, err := conn.EnableUnitFilesContext(ctx, []string{name}, false, true)
+	if err != nil {
+		return err
+	}
+
+	if len(fileChanges) != 1 {
+		return errors.New("failed to enable " + name)
+	}
+
+	return nil
+}
+
 func DisableService(name string) error {
 	// connect to systemd
 	ctx, cancel := context.WithCancel(context.Background())
