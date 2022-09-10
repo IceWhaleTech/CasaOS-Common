dcaaf381bb0dbe88e99becab20d6616c3658f8c5
Tiger Wang <tigerwang@outlook.com>
2022-09-09 21:14:21 -0400

    wip

---
+++ utils/http/multiplexer.go
@@ -0,0 +1,18 @@
+package http
+
+import (
+	"net/http"
+	"strings"
+)
+
+type HandlerMultiplexer struct {
+	handlerMap map[string]http.Handler
+}
+
+func (h *HandlerMultiplexer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	parentPath := strings.Split(strings.TrimLeft(r.URL.Path, "/"), "/")[0]
+
+	if handler, ok := h.handlerMap[parentPath]; ok {
+		handler.ServeHTTP(w, r)
+	}
+}
