0604573e0fb3488719e2778eb04290d54a249ff0
Tiger Wang <tigerwang@outlook.com>
2022-09-21 11:18:15 -0400

    wip

---
+++ utils/utils.go
@@ -0,0 +1,15 @@
+package utils
+
+func CompareSlices(a, b []interface{}) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
