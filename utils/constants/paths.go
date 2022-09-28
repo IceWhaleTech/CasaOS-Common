ad881e3f5cdf60616db3eaf5ed6f85e363564763
Tiger Wang <tigerwang@outlook.com>
2022-09-27 20:49:38 -0400

    add new constant and utils (#12)

--- utils/constants/paths.go
+++ utils/constants/paths.go
@@ -3,6 +3,7 @@ package constants
 const (
 	DefaultConfigPath  = "/etc/casaos"
 	DefaultDataPath    = "/var/lib/casaos"
+	DefaultFilePath    = "/var/lib/casaos/files"
 	DefaultRuntimePath = "/var/run/casaos"
 	DefaultLogPath     = "/var/log/casaos"
 )
---
+++ utils/utils.go
@@ -0,0 +1,29 @@
+package utils
+
+func CompareSlices(a, b []any) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func CompareStringSlices(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
