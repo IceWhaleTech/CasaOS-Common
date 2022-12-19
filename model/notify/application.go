88ae0cfe188a35c653758a924f8e83387fd6f588
Tiger Wang <tigerwang@outlook.com>
2022-12-19 21:38:50 +0000

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- model/notify/application.go
+++ model/notify/application.go
@@ -10,11 +10,12 @@
 package notify
 
 type Application struct {
-	Name     string `json:"name"`
-	State    string `json:"state"`
-	Type     string `json:"type"`
-	Icon     string `json:"icon"`
-	Message  string `json:"message"`
-	Finished bool   `json:"finished"`
-	Success  bool   `json:"success"`
+	Name       string            `json:"name"`
+	State      string            `json:"state"`
+	Type       string            `json:"type"`
+	Icon       string            `json:"icon"`
+	Message    string            `json:"message"`
+	Finished   bool              `json:"finished"`
+	Success    bool              `json:"success"`
+	Properties map[string]string `json:"properties"`
 }
