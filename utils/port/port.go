c4d2078046eff0ebab5e177facc206618022c537
Tiger Wang <tigerwang@outlook.com>
2023-04-25 12:24:14 -0400

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- utils/port/port.go
+++ utils/port/port.go
@@ -1,8 +1,15 @@
 package port
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"net"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/samber/lo"
 )
 
 // 获取可用端口
@@ -70,4 +77,50 @@ func IsPortAvailable(port int, t string) bool {
 	}
 
 	return true
+}
+
+func ListPortsInUse() ([]int, []int, error) {
+	usedPorts := map[string]map[int]struct{}{
+		"tcp": {},
+		"udp": {},
+	}
+
+	for _, protocol := range []string{"tcp", "udp"} {
+		filename := fmt.Sprintf("/proc/net/%s", protocol)
+
+		file, err := os.Open(filename)
+		if err != nil {
+			return nil, nil, errors.New("Failed to open " + filename)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			fields := strings.Fields(line)
+			if len(fields) < 2 {
+				continue
+			}
+
+			localAddress := fields[1]
+			addressParts := strings.Split(localAddress, ":")
+			if len(addressParts) < 2 {
+				continue
+			}
+
+			portHex := addressParts[1]
+			port, err := strconv.ParseInt(portHex, 16, 0)
+			if err != nil {
+				continue
+			}
+
+			usedPorts[protocol][int(port)] = struct{}{}
+		}
+
+		if err := scanner.Err(); err != nil {
+			return nil, nil, errors.New("Error reading from " + filename)
+		}
+	}
+
+	return lo.Keys(usedPorts["tcp"]), lo.Keys(usedPorts["udp"]), nil
 }
