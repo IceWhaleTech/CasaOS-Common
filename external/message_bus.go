5632d1fa81f8a207e4a4ade5f83becc84a31e0f4
Tiger Wang <tigerwang@outlook.com>
2023-08-23 17:25:13 +0800

    add method to help each CasaOS-* module to print event types as markdown (#38)

--- external/message_bus.go
+++ external/message_bus.go
@@ -1,14 +1,60 @@
 package external
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
+	"text/tabwriter"
 )
 
 const (
 	MessageBusAddressFilename = "message-bus.url"
 	APIMessageBus             = "/v2/message_bus"
 )
+
+type EventType struct {
+	Name             string
+	SourceID         string
+	PropertyTypeList []PropertyType
+}
+
+type PropertyType struct {
+	Name        string
+	Description *string
+	Example     *string
+}
+
+func PrintEventTypesAsMarkdown(sourceID, version string, eventTypes []EventType) {
+	fmt.Printf("## Source ID: `%s` (v%s)\n\n", sourceID, version)
+	for _, eventType := range eventTypes {
+		fmt.Printf("### Event Type: `%s`\n\n", eventType.Name)
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+
+		fmt.Fprintln(w, "| Property\t| Description\t| Example\t|")
+		fmt.Fprintln(w, "| --------\t| -----------\t| -------\t|")
+
+		for _, propertyType := range eventType.PropertyTypeList {
+			fmt.Fprintf(w, "| `%s`\t|", propertyType.Name)
+
+			if propertyType.Description != nil {
+				fmt.Fprintf(w, " %s", *propertyType.Description)
+			}
+
+			fmt.Fprintf(w, "\t|")
+
+			if propertyType.Example != nil {
+				fmt.Fprintf(w, " `%s`", *propertyType.Example)
+			}
+
+			fmt.Fprintln(w, "\t|")
+		}
+		w.Flush()
+
+		fmt.Println()
+	}
+}
 
 func GetMessageBusAddress(runtimePath string) (string, error) {
 	address, err := getAddress(filepath.Join(runtimePath, MessageBusAddressFilename))
