bbe41ee78dd39b823af31e01d347bf59e18899a6
LinkLeong <a624669980@163.com>
2023-10-12 12:09:01 +0100

    add field

--- model/device.go
+++ model/device.go
@@ -1,13 +1,15 @@
 package model
 
 type DeviceInfo struct {
-	LanIpv4     []string `json:"lan_ipv4"`
-	Port        int      `json:"port"`
-	DeviceName  string   `json:"device_name"`
-	DeviceModel string   `json:"device_model"`
-	Initialized bool     `json:"initialized"`
-	OS_Version  string   `json:"os_version"`
-	Hash        string   `json:"hash"`
-	RequestIp   string   `json:"request_ip,omitempty"`
-	TB_Ipv4     []string `json:"tb_ipv4"`
+	LanIpv4     []string            `json:"lan_ipv4"`
+	Port        int                 `json:"port"`
+	DeviceName  string              `json:"device_name"`
+	DeviceModel string              `json:"device_model"`
+	Initialized bool                `json:"initialized"`
+	OS_Version  string              `json:"os_version"`
+	Hash        string              `json:"hash"`
+	RequestIp   string              `json:"request_ip,omitempty"`
+	TB_Ipv4     []string            `json:"tb_ipv4"`
+	Ip4         []map[string]string `json:"ip4"`
+	TB_Ip4      []map[string]string `json:"tb_ip4"`
 }
