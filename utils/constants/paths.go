b4ed910dd031553c1baebbe5122481476ba41051
Tiger Wang <tigerwang@outlook.com>
2024-04-08 15:28:08 +0800

    wip

--- utils/constants/paths.go
+++ utils/constants/paths.go
@@ -1,9 +1,10 @@
 package constants
 
 const (
-	DefaultConfigPath  = "/etc/casaos"
-	DefaultDataPath    = "/var/lib/casaos"
-	DefaultFilePath    = "/var/lib/casaos/files"
-	DefaultRuntimePath = "/var/run/casaos"
-	DefaultLogPath     = "/var/log/casaos"
+	DefaultConfigPath   = "/etc/casaos"
+	DefaultConstantPath = "/usr/share/casaos"
+	DefaultDataPath     = "/var/lib/casaos"
+	DefaultFilePath     = "/var/lib/casaos/files"
+	DefaultRuntimePath  = "/var/run/casaos"
+	DefaultLogPath      = "/var/log/casaos"
 )
