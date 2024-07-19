463f0e6956944a1ef3295bd36c55f2849ab56ac3
orca <orca.zhang@yahoo.com>
2024-07-19 09:00:20 +0000

    fix: sonar dup lines

--- utils/exec/exec_test.go
+++ utils/exec/exec_test.go
@@ -44,7 +44,7 @@ func TestCommandContext(t *testing.T) {
 			err: true,
 		},
 		{
-			name: "Test CommandContext with error",
+			name: "Test CommandContext with injection",
 			args: args{
 				ctx:  context.Background(),
 				name: "ls",
@@ -53,7 +53,7 @@ func TestCommandContext(t *testing.T) {
 			err: true,
 		},
 		{
-			name: "Test CommandContext with error",
+			name: "Test CommandContext with multiple injection",
 			args: args{
 				ctx:  context.Background(),
 				name: "ls",
@@ -62,7 +62,7 @@ func TestCommandContext(t *testing.T) {
 			err: true,
 		},
 		{
-			name: "Test CommandContext with error",
+			name: "Test CommandContext with multiple injection 2",
 			args: args{
 				ctx:  context.Background(),
 				name: "ls",
@@ -71,7 +71,7 @@ func TestCommandContext(t *testing.T) {
 			err: true,
 		},
 		{
-			name: "Test CommandContext with error",
+			name: "Test CommandContext with injection on name",
 			args: args{
 				ctx:  context.Background(),
 				name: "ls ;whoami",
@@ -80,7 +80,7 @@ func TestCommandContext(t *testing.T) {
 			err: true,
 		},
 		{
-			name: "Test CommandContext with error",
+			name: "Test CommandContext with injection on arg-1",
 			args: args{
 				ctx:  context.Background(),
 				name: "ls",
@@ -89,7 +89,7 @@ func TestCommandContext(t *testing.T) {
 			err: true,
 		},
 		{
-			name: "Test CommandContext with error",
+			name: "Test CommandContext with quotation injection",
 			args: args{
 				ctx:  context.Background(),
 				name: "ls",
@@ -98,16 +98,7 @@ func TestCommandContext(t *testing.T) {
 			err: true,
 		},
 		{
-			name: "Test CommandContext with error",
-			args: args{
-				ctx:  context.Background(),
-				name: "ls",
-				args: []string{"-l;whoami"},
-			},
-			err: true,
-		},
-		{
-			name: "Test CommandContext with no error",
+			name: "Test CommandContext with injection shell script",
 			args: args{
 				ctx:  context.Background(),
 				name: "source /etc/local-storage-helper.sh ;USB_Stop_Auto",
@@ -115,7 +106,7 @@ func TestCommandContext(t *testing.T) {
 			err: true,
 		},
 		{
-			name: "Test CommandContext with no error",
+			name: "Test CommandContext with injection shell script divided args",
 			args: args{
 				ctx:  context.Background(),
 				name: "source",
@@ -128,7 +119,7 @@ func TestCommandContext(t *testing.T) {
 			err: true,
 		},
 		{
-			name: "Test CommandContext with no error",
+			name: "Test CommandContext with new-line injection shell script",
 			args: args{
 				ctx:  context.Background(),
 				name: "source /etc/local-storage-helper.sh\nenv",
