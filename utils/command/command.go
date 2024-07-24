82e3f1c5355b14e5f4f056171b0cb108bee37558
Ns2Kracy <89824014+Ns2Kracy@users.noreply.github.com>
2024-07-24 16:48:13 +0800

    feat: add command (#58)

---
+++ utils/command/command.go
@@ -0,0 +1,128 @@
+package command
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	exec2 "github.com/IceWhaleTech/CasaOS-Common/utils/exec"
+)
+
+// Deprecated: This method is not safe, sould have ensure input.
+func OnlyExec(cmdStr string) (string, error) {
+	cmd := exec.Command("/bin/bash", "-c", cmdStr)
+	println(cmd.String())
+	buf, err := cmd.CombinedOutput()
+	println(string(buf))
+	return string(buf), err
+}
+
+func ExecResultStr(cmdStr string) (string, error) {
+	cmds := strings.Fields(cmdStr)
+	cmd := exec2.Command(cmds[0], cmds[1:]...)
+	println(cmd.String())
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+
+	defer stdout.Close()
+	if err = cmd.Start(); err != nil {
+		return "", err
+	}
+
+	buf, err := io.ReadAll(stdout)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buf), cmd.Wait()
+}
+
+func ExecResultStrArray(cmdStr string) ([]string, error) {
+	cmds := strings.Fields(cmdStr)
+	cmd := exec2.Command(cmds[0], cmds[1:]...)
+
+	println(cmd.String())
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	defer stdout.Close()
+
+	if err = cmd.Start(); err != nil {
+		return nil, err
+	}
+
+	buf := []string{}
+	outputBuf := bufio.NewReader(stdout)
+	for {
+		output, _, err := outputBuf.ReadLine()
+		if err != nil {
+			if err.Error() != "EOF" {
+				fmt.Printf("Error :%s\n", err)
+			}
+			break
+		}
+		buf = append(buf, string(output))
+	}
+
+	return buf, cmd.Wait()
+}
+
+func ExecuteScripts(scriptDirectory string) error {
+	if _, err := os.Stat(scriptDirectory); os.IsNotExist(err) {
+		fmt.Printf("No post-start scripts at %s\n", scriptDirectory)
+		return err
+	}
+
+	files, err := os.ReadDir(scriptDirectory)
+	if err != nil {
+		fmt.Printf("Failed to read from script directory %s: %s\n", scriptDirectory, err.Error())
+		return err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		scriptFilepath := filepath.Join(scriptDirectory, file.Name())
+
+		f, err := os.Open(scriptFilepath)
+		if err != nil {
+			fmt.Printf("Failed to open script file %s: %s\n", scriptFilepath, err.Error())
+			continue
+		}
+		f.Close()
+
+		scanner := bufio.NewScanner(f)
+		scanner.Scan()
+		shebang := scanner.Text()
+
+		interpreter := "/bin/sh"
+		if strings.HasPrefix(shebang, "#!") {
+			interpreter = shebang[2:]
+		}
+
+		cmd := exec2.Command(interpreter, scriptFilepath)
+
+		fmt.Printf("Executing post-start script %s using %s\n", scriptFilepath, interpreter)
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		err = cmd.Run()
+		if err != nil {
+			fmt.Printf("Failed to execute post-start script %s: %s\n", scriptFilepath, err.Error())
+			return err
+		}
+	}
+	fmt.Println("Finished executing post-start scripts.")
+	return nil
+}
---
+++ utils/command/command_test.go
@@ -0,0 +1,109 @@
+package command
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCommand(t *testing.T) {
+	tests := []struct {
+		name   string
+		cmdStr string
+		noErr  bool
+	}{
+		{
+			name:   "Test Command",
+			cmdStr: "ls -l",
+			noErr:  true,
+		},
+		{
+			name:   "Test Command with noescape",
+			cmdStr: "ls -l whoami",
+		},
+		{
+			name:   "Test Command with error",
+			cmdStr: "`whoami` -l /test",
+		},
+		{
+			name:   "Test Command with injection",
+			cmdStr: "ls -l `whoami`",
+		},
+		{
+			name:   "Test Command with multiple injection",
+			cmdStr: "ls -l ; whoami",
+		},
+		{
+			name:   "Test Command with multiple injection 2",
+			cmdStr: "ls -l ; whoami",
+		},
+		{
+			name:   "Test Command with injection on name",
+			cmdStr: "ls ;whoami -l",
+		},
+		{
+			name:   "Test Command with injection on arg-1",
+			cmdStr: "ls -l;whoami",
+		},
+		{
+			name:   "Test Command with quotation injection",
+			cmdStr: "ls -l \" \"whoami",
+		},
+		{
+			name:   "Test Command with injection shell script",
+			cmdStr: "source /etc/local-storage-helper.sh ;USB_Stop_Auto",
+		},
+		{
+			name:   "Test Command with injection shell script divided args",
+			cmdStr: "source /etc/local-storage-helper.sh ; USB_Stop_Auto",
+		},
+		{
+			name:   "Test Command with new-line injection shell script",
+			cmdStr: "source /etc/local-storage-helper.sh\nenv",
+		},
+	}
+
+	t.Run("TestExecuteScripts", func(t *testing.T) {
+		// make a temp directory
+		tmpDir, err := os.MkdirTemp("", "casaos-test-*")
+		if err != nil {
+			t.Error(err)
+		}
+		defer os.RemoveAll(tmpDir)
+
+		ExecuteScripts(tmpDir)
+
+		// create a sample script under tmpDir
+		script := tmpDir + "/test.sh"
+		f, err := os.Create(script)
+		if err != nil {
+			t.Error(err)
+		}
+		defer f.Close()
+
+		// write a sample script
+		_, err = f.WriteString("#!/bin/bash\necho 123")
+		if err != nil {
+			t.Error(err)
+		}
+
+		ExecuteScripts(tmpDir)
+	})
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if output, err := ExecResultStr(tt.cmdStr); tt.noErr != (err == nil) {
+				t.Errorf("ExecResultStr() error = %v, wantErr %v", err, tt.noErr)
+			} else {
+				t.Logf("Output: %s", output)
+			}
+		})
+
+		t.Run(tt.name, func(t *testing.T) {
+			if output, error := ExecResultStrArray(tt.cmdStr); tt.noErr != (error == nil) {
+				t.Errorf("ExecResultStrArray() error = %v, wantErr %v", error, tt.noErr)
+			} else {
+				t.Logf("Output: %v", output)
+			}
+		})
+	}
+}
