92cdae26928b8fafe5b208522a2a31dc1d765ae9
CorrectRoad <a778917369@gmail.com>
2025-03-20 11:59:17 +0800

    feat: throttle

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"sync/atomic"
+	"time"
+)
+
 func CompareSlices(a, b []any) bool {
 	if len(a) != len(b) {
 		return false
@@ -30,4 +35,33 @@ func CompareStringSlices(a, b []string) bool {
 
 func Ptr[T any](v T) *T {
 	return &v
+}
+
+type Throttle struct {
+	lastCall time.Time
+	limit    time.Duration
+	timer    atomic.Pointer[time.Timer]
+}
+
+func NewThrottle(limit time.Duration) *Throttle {
+	return &Throttle{limit: limit}
+}
+
+func (t *Throttle) Do(f func()) {
+	now := time.Now()
+	if now.Sub(t.lastCall) >= t.limit {
+		t.lastCall = now
+		f()
+		if old := t.timer.Swap(nil); old != nil {
+			old.Stop()
+		}
+	} else {
+		lastFunc := f
+		if old := t.timer.Swap(time.AfterFunc(t.limit-now.Sub(t.lastCall), func() {
+			t.lastCall = time.Now()
+			lastFunc()
+		})); old != nil {
+			old.Stop()
+		}
+	}
 }
